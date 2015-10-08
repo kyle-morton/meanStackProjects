a5fabfb6db06ffa505730a9f573e855040f64f7e
kylemorton5770 <michael.morton@smail.astate.edu>
2015-10-08 16:57:31 -0500

    moved node routing to own config file

--- mean_stack_app/config.js
+++ mean_stack_app/config.js
@@ -1,3 +1,7 @@
+//REM: module.exports is the way we pass info back and forth between js files!
+//module.exports at beginning of app is {}, but after we use require() and add
+//properties to the exports{} object, we fill the object with useful data and settings
+
 module.exports = {
 	'port' : process.env.PORT || 8080, // set the port for our app
 	'secret' : 'superSecretPasswordsecretThing', //used in creation of JWT tokens
